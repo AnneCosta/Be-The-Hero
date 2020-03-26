281114edbae1663df07952abd9ac2feff02d8a17
Anne <fabiannecs@hotmail.com>
2020-03-26 00:19:06 -0300

    Atualização de arquivos

--- backend/src/database/migrations/20200325211640_create_incidents.js
+++ backend/src/database/migrations/20200325211640_create_incidents.js
@@ -8,8 +8,8 @@ exports.up = function(knex) {
         table.decimal('value').notNullable();
 
         table.string('ong_id').notNullable();
-
         table.foreign('ong_id').references('id').inTable('ongs');
+        
       });
 };
 
